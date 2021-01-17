c6e203f6e26e19a5955d91a80c85d21f68ef20a0
Valeriy-G-Abashin <77538913+Valeriy-G-Abashin@users.noreply.github.com>
2021-01-17 19:19:06 +0300

    Reject stdio.h

--- time_rus.c
+++ time_rus.c
@@ -21,7 +21,10 @@
 
 void strCpy(char *dest, const char *src);
 void strCat(char *dest, const char *src);
-void addValueWithZero(char * buf, int value);
+int strToInt(const char *value);
+char * intToStr(const int value, char *str);
+int digit(const int value);
+void addValueWithZero(char *buf, const int value);
 
 /* Получение текущего времени */
 time_t getCurrentTt(void) {
@@ -44,7 +47,8 @@ clock_t getClock(void) {
 }
 
 /* Сравнение процессорных тиков */
-clock_t diffClock(clock_t first, clock_t second, clock_t *clocks_per_sec) {
+clock_t diffClock(const clock_t first, const clock_t second,
+		clock_t *clocks_per_sec) {
 	*clocks_per_sec = CLOCKS_PER_SEC;
 	if (second >= first)
 		return second-first;
@@ -52,7 +56,7 @@ clock_t diffClock(clock_t first, clock_t second, clock_t *clocks_per_sec) {
 }
 
 /* Получение разности между временем tt/tm */
-double diffTimeTt(time_t first_tt, time_t second_tt) {
+double diffTimeTt(const time_t first_tt, const time_t second_tt) {
 	return difftime(second_tt, first_tt);
 }
 double diffTimeTm(struct tm *first_tm, struct tm *second_tm) {
@@ -62,7 +66,7 @@ double diffTimeTm(struct tm *first_tm, struct tm *second_tm) {
 }
 
 /* Преобразование time_t в struct tm(localtime) */
-struct tm * ttToLocalTm(time_t *t) {
+struct tm * ttToLocalTm(const time_t *t) {
 	return localtime(t);
 }
 /* Преобразование struct tm(localtime) в time_t */
@@ -70,25 +74,66 @@ time_t localTmToTt(struct tm *st_tm) {
 	return mktime(st_tm);
 }
 /* Преобразование time_t в struct tm(gmtine) */
-struct tm * ttToGmTm(time_t *t) {
+struct tm * ttToGmTm(const time_t *t) {
 	return gmtime(t);
 }
 /* Преобразование struct tm(gmtime) в time_t */
 time_t gmTmToTt(struct tm *st_tm) {
-	return timegm(st_tm);
+	time_t result = 0;
+	/* Подсчет секунд в полных годах */
+	int year = 1970;
+	int cur_year = st_tm->tm_year + 1900;
+	for ( ; year < cur_year; year++) {
+		if (year % 4 == 0)
+			result += 31622400; /* 31622400 - год 366 дней */
+		else
+			result += 31536000; /* 31536000 - год 365 дней */
+	}
+	cur_year++;
+	/* дней в году, часов, минут, секунд */
+	result += st_tm->tm_yday * 86400 +
+		st_tm->tm_hour * 3600 +
+		st_tm->tm_min * 60 +
+		st_tm->tm_sec;
+	return result;
 }
 
-/* Преобразование YYYY.MM.DD HH:MM:SS в struct tm */
-struct tm * stringToTm(char *buf) {
+/* Преобразование YYYY.MM.DD HH:MM.SS в struct tm */
+struct tm * stringToTm(const char *buf) {
 	int year, mon, day, hour, min, sec;
 	year = mon = day = hour = min = sec = 0;
-	sscanf(buf, "%d.%d.%d %d:%d:%d", &year, &mon, &day,
-			&hour, &min, &sec);
+	char temp[10] = {0};
+	temp[0] = buf[0];
+	temp[1] = buf[1];
+	temp[2] = buf[2];
+	temp[3] = buf[3];
+	temp[4] = 0;
+	year = strToInt(temp);
+	temp[0] = buf[5];
+	temp[1] = buf[6];
+	temp[2] = 0;
+	mon = strToInt(temp);
+	temp[0] = buf[8];
+	temp[1] = buf[9];
+	temp[2] = 0;
+	day = strToInt(temp);
+	temp[0] = buf[11];
+	temp[1] = buf[12];
+	temp[2] = 0;
+	hour = strToInt(temp);
+	temp[0] = buf[14];
+	temp[1] = buf[15];
+	temp[2] = 0;
+	min = strToInt(temp);
+	temp[0] = buf[17];
+	temp[1] = buf[18];
+	temp[2] = 0;
+	sec = strToInt(temp);
 	return YMDHMStoTM(year, mon, day, hour, min, sec);
 }
 
-struct tm * YMDHMStoTM(int year, int mon, int day,
-		int hour, int min, int sec) {
+struct tm * YMDHMStoTM(const int year, const int mon, const int day,
+		const int hour, const int min, const int sec) {
 	time_t t = time(NULL);
 	struct tm *st_tm = ttToLocalTm(&t);
 	st_tm->tm_year = year - 1900;
@@ -101,27 +146,183 @@ struct tm * YMDHMStoTM(int year, int mon, int day,
 }
 
 /* Преобразование struct tm в YYYY.MM.DD HH:MM:SS*/
-char * tmToString(struct tm * st_tm, char * buf, int32_t buf_len) {
+char * tmToString(struct tm * st_tm, char * buf, const int32_t buf_len) {
 	if (buf_len < 40)
 		return NULL;
 	char temp[10] = {0};
-	char zero[4] = "0";
-	sprintf(temp, "%d.", st_tm->tm_year + 1900);
+	char dot[] = ".";
+	intToStr(st_tm->tm_year + 1900, temp);
 	strCpy(buf, temp);
+	strCat(buf, dot);
 	addValueWithZero(buf, st_tm->tm_mon + 1);
+	strCat(buf, dot);
 	addValueWithZero(buf, st_tm->tm_mday);
+	strCat(buf, " ");
 	addValueWithZero(buf, st_tm->tm_hour);
+	strCat(buf, ":");
 	addValueWithZero(buf, st_tm->tm_min);
+	strCat(buf, dot);
 	addValueWithZero(buf, st_tm->tm_sec);
 	return buf;
 }
 
-void addValueWithZero(char * buf, int value) {
+/* Преобразование time_t в YYYY.MM.DD HH:MM:SS*/
+char * ttToLocalString(const time_t *t, char *buf, const int32_t buf_len) {
+	return tmToString(ttToLocalTm(t), buf, buf_len);
+}
+
+char * ttToGmString(const time_t *t, char *buf, const int32_t buf_len) {
+	return tmToString(ttToGmTm(t), buf, buf_len);
+}
+
+/* функция получение из time_t локальный */
+int getLocalSec(const time_t tt) { /* секунды (0-60) */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_sec;
+}
+int getLocalMin(const time_t tt) { /* минуты (0-59) */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_min;
+}
+int getLocalHour(const time_t tt) { /* часы (0-23) */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_hour;
+}
+int getLocalMday(const time_t tt) { /* день в месяце (1-31) */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_mday;
+}
+int getLocalMon(const time_t tt) { /* месяц (0-11) */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_mon;
+}
+int getLocalYear(const time_t tt) { /* год */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_year + 1900;
+}
+int getLocalWday(const time_t tt) { /* день недели (0-6, воскресенье = 0) */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_wday;
+}
+int getLocalYday(const time_t tt) { /* день в году (0-365, 1 январь = 0) */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_yday;
+}
+int getLocalDst(const time_t tt) { /* летнее время */
+	struct tm * stm = ttToLocalTm(&tt);
+	return stm->tm_isdst;
+}
+
+/* функция получение из time_t GM */
+int getGmSec(const time_t tt) { /* секунды (0-60) */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_sec;
+}
+int getGmMin(const time_t tt) { /* минуты (0-59) */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_min;
+}
+int getGmHour(const time_t tt) { /* часы (0-23) */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_hour;
+}
+int getGmMday(const time_t tt) { /* день в месяце (1-31) */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_mday;
+}
+int getGmMon(const time_t tt) { /* месяц (0-11) */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_mon;
+}
+int getGmYear(const time_t tt) { /* год - 1900 */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_year + 1900;
+}
+int getGmWday(const time_t tt) { /* день недели (0-6, воскресенье = 0) */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_wday;
+}
+int getGmYday(const time_t tt) { /* день в году (0-365, 1 январь = 0) */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_yday;
+}
+int getGmDst(const time_t tt) { /* летнее время */
+	struct tm * stm = ttToGmTm(&tt);
+	return stm->tm_isdst;
+}
+
+/* Library function */
+char * intToStr(const int value, char *str) {
+	/* получить все разряды */
+	int value_ = value;
+	int count = 0;
+	do {
+		switch (value_ % 10) {
+			case 0: str[count] = '0'; break;
+			case 1: str[count] = '1'; break;
+			case 2: str[count] = '2'; break;
+			case 3: str[count] = '3'; break;
+			case 4: str[count] = '4'; break;
+			case 5: str[count] = '5'; break;
+			case 6: str[count] = '6'; break;
+			case 7: str[count] = '7'; break;
+			case 8: str[count] = '8'; break;
+			case 9: str[count] = '9'; break;
+		}
+		count++;
+	} while ((value_ /= 10) != 0);
+	str[count] = 0;
+	/* Перевернуть число в правильный порядок */
+	char temp;
+	int top, bottom;
+	top = count - 1;
+	bottom = 0;
+	for ( ; bottom < top; top--, bottom++) {
+		temp = str[top];
+		str[top] = str[bottom];
+		str[bottom] = temp;
+	}
+	return str;
+}
+
+int digit(const int value) {
+	int result = 1;
+	int count = 0;
+	for (count = 0; count < value; count++)
+		result *= 10;
+	return result;
+}
+
+int strToInt(const char *value) {
+	int result = 0;
+	int count = 0;
+	while (value[count] != 0)
+		count++;
+	if (count)
+		count--;
+	int count_digit = 0;
+	for ( ; count >= 0; count--) {
+		switch(value[count]) {
+			case '1': result += 1 * digit(count_digit); break;
+			case '2': result += 2 * digit(count_digit); break;
+			case '3': result += 3 * digit(count_digit); break;
+			case '4': result += 4 * digit(count_digit); break;
+			case '5': result += 5 * digit(count_digit); break;
+			case '6': result += 6 * digit(count_digit); break;
+			case '7': result += 7 * digit(count_digit); break;
+			case '8': result += 8 * digit(count_digit); break;
+			case '9': result += 9 * digit(count_digit); break;
+		}
+		count_digit++;
+	}
+	return result;
+}
+
+void addValueWithZero(char *buf, const int value) {
 	char temp[10] = {0};
-	char zero[4] = "0";
 	if (value < 10)
-		strCat(buf, zero);
-	sprintf(temp, "%d:", value);
+		strCat(buf, "0");
+	intToStr(value, temp);
 	strCat(buf, temp);
 }
 
@@ -145,90 +346,5 @@ void strCat(char *dest, const char *src) {
 		count_dest++;
 	}
 	dest[count_dest] = 0;
-}
-
-/* Преобразование time_t в YYYY.MM.DD HH:MM:SS*/
-char * ttToLocalString(time_t *t, char *buf, int32_t buf_len) {
-	return tmToString(ttToLocalTm(t), buf, buf_len);
-}
-
-char * ttToGmString(time_t *t, char *buf, int32_t buf_len) {
-	return tmToString(ttToGmTm(t), buf, buf_len);
-}
-
-/* функция получение из time_t локальный */
-int getLocalSec(time_t tt) { /* секунды (0-60) */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_sec;
-}
-int getLocalMin(time_t tt) { /* минуты (0-59) */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_min;
-}
-int getLocalHour(time_t tt) { /* часы (0-23) */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_hour;
-}
-int getLocalMday(time_t tt) { /* день в месяце (1-31) */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_mday;
-}
-int getLocalMon(time_t tt) { /* месяц (0-11) */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_mon;
-}
-int getLocalYear(time_t tt) { /* год */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_year + 1900;
-}
-int getLocalWday(time_t tt) { /* день недели (0-6, воскресенье = 0) */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_wday;
-}
-int getLocalYday(time_t tt) { /* день в году (0-365, 1 январь = 0) */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_yday;
-}
-int getLocalDst(time_t tt) { /* летнее время */
-	struct tm * stm = ttToLocalTm(&tt);
-	return stm->tm_isdst;
-}
-
-/* функция получение из time_t GM */
-int getGmSec(time_t tt) { /* секунды (0-60) */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_sec;
-}
-int getGmMin(time_t tt) { /* минуты (0-59) */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_min;
-}
-int getGmHour(time_t tt) { /* часы (0-23) */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_hour;
-}
-int getGmMday(time_t tt) { /* день в месяце (1-31) */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_mday;
-}
-int getGmMon(time_t tt) { /* месяц (0-11) */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_mon;
-}
-int getGmYear(time_t tt) { /* год - 1900 */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_year + 1900;
-}
-int getGmWday(time_t tt) { /* день недели (0-6, воскресенье = 0) */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_wday;
-}
-int getGmYday(time_t tt) { /* день в году (0-365, 1 январь = 0) */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_yday;
-}
-int getGmDst(time_t tt) { /* летнее время */
-	struct tm * stm = ttToGmTm(&tt);
-	return stm->tm_isdst;
 }
 
--- time_rus.h
+++ time_rus.h
@@ -15,13 +15,12 @@
    <http://www.gnu.org/licenses/>.  */
 
 /* NOTES:
-time_t always GM. Localtime/GM only struct tm
+time_t always GM. Localtime/GM only struct tm.
 system struct tm * - only one for thread!
 tt(Tt) - time_t
 tm(Tm) - struct tm
 string(String) - char *
 */
-#include <stdio.h>
 #include <time.h>
 #include <stdint.h>
 
@@ -35,51 +34,51 @@ struct tm * getCurrentGmTm(void);
 
 /* Получение разности между временными метками */
 /* ПРЕДУПРЕЖДЕНИЕ: Не сравнивать местное и GM время */
-double diffTimeTt(time_t first_tt, time_t second_tt);
+double diffTimeTt(const time_t first_tt, const time_t second_tt);
 double diffTimeTm(struct tm *first_tm, struct tm *second_tm);
 
 /* Получение процессорных тиков. Использовать в пределах секунды. */
 clock_t getClock(void);
 /* Сравнение процессорных тиков */
-clock_t diffClock(clock_t first, clock_t second, clock_t *clocks_per_sec);
+clock_t diffClock(const clock_t first, const clock_t second,
+	clock_t *clocks_per_sec);
 
 /* Преобразование time_t в struct tm(localtime) */
-struct tm * ttToLocalTm(time_t *t);
+struct tm * ttToLocalTm(const time_t *t);
 /* Преобразование struct tm(localtime) в time_t */
 time_t localTmToTt(struct tm *st_tm);
 /* Преобразование time_t в struct tm(gmtine) */
-struct tm * ttToGmTm(time_t *t);
+struct tm * ttToGmTm(const time_t *t);
 /* Преобразование struct tm(gmtime) в time_t */
 time_t gmTmToTt(struct tm *st_tm);
 /* Преобразование YYYY.MM.DD HH:MM:SS в struct tm */
-struct tm * stringToTm(char *buf);
-struct tm * YMDHMStoTM(int year, int mon, int day,
-		int hour, int min, int sec);
+struct tm * stringToTm(const char *buf);
+struct tm * YMDHMStoTM(const int year, const int mon, const int day,
+		const int hour, const int min, const int sec);
 /* Преобразование struct tm в YYYY.MM.DD HH:MM:SS*/
-char * tmToString(struct tm * st_tm, char * buf, int32_t buf_len);
+char * tmToString(struct tm * st_tm, char * buf, const int32_t buf_len);
 /* Преобразование time_t в YYYY.MM.DD HH:MM:SS*/
-char * ttToLocalString(time_t *t, char *buf, int32_t buf_len);
-char * ttToGmString(time_t *t, char *buf, int32_t buf_len);
+char * ttToLocalString(const time_t *t, char *buf, const int32_t buf_len);
+char * ttToGmString(const time_t *t, char *buf, const int32_t buf_len);
 
 /* функция получение из time_t локальный */
-int getLocalSec(time_t tt); /* секунды (0-60) */
-int getLocalMin(time_t tt); /* минуты (0-59) */
-int getLocalHour(time_t tt); /* часы (0-23) */
-int getLocalMday(time_t tt); /* день в месяце (1-31) */
-int getLocalMon(time_t tt); /* месяц (0-11) */
-int getLocalYear(time_t tt); /* год */
-int getLocalWday(time_t tt); /* день недели (0-6, воскресенье = 0) */
-int getLocalYday(time_t tt); /* день в году (0-365, 1 январь = 0) */
-int getLocalDst(time_t tt); /* летнее время */
-
+int getLocalSec(const time_t tt); /* секунды (0-60) */
+int getLocalMin(const time_t tt); /* минуты (0-59) */
+int getLocalHour(const time_t tt); /* часы (0-23) */
+int getLocalMday(const time_t tt); /* день в месяце (1-31) */
+int getLocalMon(const time_t tt); /* месяц (0-11) */
+int getLocalYear(const time_t tt); /* год */
+int getLocalWday(const time_t tt); /* день недели (0-6, воскресенье = 0) */
+int getLocalYday(const time_t tt); /* день в году (0-365, 1 январь = 0) */
+int getLocalDst(const time_t tt); /* летнее время */
 /* функция получение из time_t GM */
-int getGmSec(time_t tt); /* секунды (0-60) */
-int getGmMin(time_t tt); /* минуты (0-59) */
-int getGmHour(time_t tt); /* часы (0-23) */
-int getGmMday(time_t tt); /* день в месяце (1-31) */
-int getGmMon(time_t tt); /* месяц (0-11) */
-int getGmYear(time_t tt); /* год */
-int getGmWday(time_t tt); /* день недели (0-6, воскресенье = 0) */
-int getGmYday(time_t tt); /* день в году (0-365, 1 январь = 0) */
-int getGmDst(time_t tt); /* летнее время */
+int getGmSec(const time_t tt); /* секунды (0-60) */
+int getGmMin(const time_t tt); /* минуты (0-59) */
+int getGmHour(const time_t tt); /* часы (0-23) */
+int getGmMday(const time_t tt); /* день в месяце (1-31) */
+int getGmMon(const time_t tt); /* месяц (0-11) */
+int getGmYear(const time_t tt); /* год */
+int getGmWday(const time_t tt); /* день недели (0-6, воскресенье = 0) */
+int getGmYday(const time_t tt); /* день в году (0-365, 1 январь = 0) */
+int getGmDst(const time_t tt); /* летнее время */
 
